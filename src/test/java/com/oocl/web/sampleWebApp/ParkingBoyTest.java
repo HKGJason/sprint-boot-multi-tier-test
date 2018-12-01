78ab18dbb5c7d49f8a3ce52c369c925861302612
J <jason19951020@gmail.com>
2018-12-01 17:46:44 +0800

    test: all test for get parkingboys

--- src/test/java/com/oocl/web/sampleWebApp/ParkingBoyTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/ParkingBoyTest.java
@@ -1,12 +1,21 @@
 package com.oocl.web.sampleWebApp;
 
+import com.oocl.web.sampleWebApp.domain.ParkingBoy;
 import com.oocl.web.sampleWebApp.domain.ParkingBoyRepository;
+import com.oocl.web.sampleWebApp.models.ParkingBoyResponse;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.oocl.web.sampleWebApp.WebTestUtil.getContentAsObject;
+import static junit.framework.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,5 +27,14 @@ public class ParkingBoyTest {
     @Autowired
     private MockMvc mvc;
 
-    
+    @Test
+    public void parking_boy_get_test() throws Exception{
+        //given
+        ParkingBoy boy = parkingBoyRepository.save(new ParkingBoy("Test1"));
+        //when
+        MvcResult result = mvc.perform(get("/parkingboys")).andReturn();
+        //then
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("Test1", getContentAsObject(result, ParkingBoyResponse[].class)[0].getEmployeeId());
+    }
 }
